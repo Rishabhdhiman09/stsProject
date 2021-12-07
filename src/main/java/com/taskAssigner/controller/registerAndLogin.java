aedbc220b85828f862b0a1b3b496f71c220b6bcd
rishabhdhiman09 <rishabhdhiman09@gmail.com>
2021-12-07 22:06:32 +0530

    jwt filter added

--- src/main/java/com/taskAssigner/controller/registerAndLogin.java
+++ src/main/java/com/taskAssigner/controller/registerAndLogin.java
@@ -2,19 +2,15 @@ package com.taskAssigner.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.taskAssigner.model.AuthenticateRequest;
-import com.taskAssigner.model.AuthenticateResponse;
 import com.taskAssigner.model.User;
 import com.taskAssigner.service.JwtUtil;
 import com.taskAssigner.service.UserService;
@@ -25,14 +21,6 @@ public class registerAndLogin {
 	@Autowired
 	UserService userService;
 	
-	@Autowired
-	AuthenticationManager authenticationManager;
-	
-	@Autowired
-	UserDetailsService userDetailsService;
-	
-	@Autowired
-	JwtUtil jwtUtil;
 	
 	@PostMapping("/registering")
 	public String register(@ModelAttribute User user, Model model) {
@@ -44,20 +32,13 @@ public class registerAndLogin {
 	@PostMapping("/authenticate")
 	public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticateRequest authenticateRequest) throws Exception{
 		
-		try {
-			authenticationManager.authenticate(
-					new UsernamePasswordAuthenticationToken(authenticateRequest.getUserName(), authenticateRequest.getPassword())
-				);
-		}catch (BadCredentialsException e) {
-			throw new Exception("Incorrect username and password", e);
-		}
-		
-		final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticateRequest.getUserName());
-		
-		final String jwt = jwtUtil.generateToken(userDetails);
-		
-		return ResponseEntity.ok(new AuthenticateResponse(jwt));
+		return userService.authenticate(authenticateRequest);
 		
 		
+	}
+	
+	@GetMapping("/success")
+	public String success() {
+		return "success";
 	}
 }
--- src/main/java/com/taskAssigner/service/UserService.java
+++ src/main/java/com/taskAssigner/service/UserService.java
@@ -1,24 +1,47 @@
 package com.taskAssigner.service;
 
 import java.util.ArrayList;
+
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 
 import com.taskAssigner.Repository.UserRepository;
+import com.taskAssigner.model.AuthenticateRequest;
+import com.taskAssigner.model.AuthenticateResponse;
 import com.taskAssigner.model.Role;
 import com.taskAssigner.model.Task;
 import com.taskAssigner.model.User;
 import com.taskAssigner.util.Constants;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Service
 public class UserService {
 	
 	@Autowired
 	UserRepository userRepository;
+	
+	@Autowired
+	AuthenticationManager authenticationManager;
+	
+	@Autowired
+	UserDetailsService userDetailsService;
+	
+	@Autowired
+	JwtUtil jwtUtil;
 	
 	public String addUser(User user) {
 		try {
@@ -37,18 +60,35 @@ public class UserService {
 		}
 		
 	}
-	
-	public boolean autheticateUser(String email, String password) {
+
+	public ResponseEntity<?> authenticate(AuthenticateRequest authenticateRequest) throws Exception {
+		// TODO Auto-generated method stub
 		try {
-			User user = userRepository.findByEmail(email);
-			if (user != null) {
-				return true;
-			}
-		}catch (Exception e) {
-			// logger will be added later
+			authenticationManager.authenticate(
+					new UsernamePasswordAuthenticationToken(authenticateRequest.getUserName(), authenticateRequest.getPassword())
+				);
+		}catch (BadCredentialsException e) {
+			throw new Exception("Incorrect username and password", e);
 		}
-		return false;
 		
+		final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticateRequest.getUserName());
+		
+		final String jwt = jwtUtil.generateToken(userDetails);
+		
+		return ResponseEntity.ok(new AuthenticateResponse(jwt));
 	}
+	
+//	public boolean autheticateUser(String email, String password) {
+//		try {
+//			User user = userRepository.findByEmail(email);
+//			if (user != null) {
+//				return true;
+//			}
+//		}catch (Exception e) {
+//			// logger will be added later
+//		}
+//		return false;
+//		
+//	}
 }
 
